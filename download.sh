44857504578bd622b79d39a59c6f8ff57227295f
mmmmm-w <2311602492@qq.com>
2025-11-19 11:11:26 -0600

    fix download.sh

--- download.sh
+++ download.sh
@@ -1,8 +1,8 @@
 # download openface weights
 mkdir weights
 cd weights
-wget https://huggingface.co/nutPace/openface_weights/resolve/main/Alignment_RetinaFace.pth?download=true
-wget https://huggingface.co/nutPace/openface_weights/resolve/main/Landmark_68.pkl?download=true
-wget https://huggingface.co/nutPace/openface_weights/resolve/main/MTL_backbone.pth?download=true
-wget https://huggingface.co/nutPace/openface_weights/resolve/main/mobilenetV1X0.25_pretrain.tar?download=true
-wget https://huggingface.co/nutPace/openface_weights/resolve/main/Landmark_68.pkl?download=true
+wget https://huggingface.co/nutPace/openface_weights/resolve/main/Alignment_RetinaFace.pth
+wget https://huggingface.co/nutPace/openface_weights/resolve/main/Landmark_68.pkl
+wget https://huggingface.co/nutPace/openface_weights/resolve/main/MTL_backbone.pth
+wget https://huggingface.co/nutPace/openface_weights/resolve/main/mobilenetV1X0.25_pretrain.tar
+wget https://huggingface.co/nutPace/openface_weights/resolve/main/Landmark_68.pkl
