fad989160c4f350e274d487f879010787b6d451b
mmmmm-w <2311602492@qq.com>
2025-11-30 22:04:33 -0600

    updates

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,7 +1,8 @@
 python scripts/train_view_model.py \
   --data-root data \
   --output weights/view_mtl.pth \
-  --epochs 40 \
+  --epochs 50 \
   --batch-size 8 \
   --lr 0.00005 \
+  --val-split 0.1 \
 # --train-backbone # if you want to finetune the whole model
